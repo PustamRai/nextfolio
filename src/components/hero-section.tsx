628c9e9b0db472c6c0ebb85cf7cdc0d0a363f8b8
Pustam Rai <github.pustam@gmail.com>
2025-08-17 18:14:32 +0545

    update UI and add link to social media

--- src/components/hero-section.tsx
+++ src/components/hero-section.tsx
@@ -1,9 +1,10 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-import { ArrowUpCircle, Github, Linkedin, Mail } from "lucide-react";
+import { Github, Linkedin, Mail } from "lucide-react";
 import Link from "next/link";
-import { motion, Variants } from "framer-motion";
+import { motion, type Variants } from "framer-motion";
+import Image from "next/image";
 
 export default function HeroSection() {
    const containerVariants: Variants = {
@@ -41,31 +42,30 @@ export default function HeroSection() {
       },
    };
 
-   const floatingVariants: Variants = {
-      animate: {
-         y: [0, -10, 0],
-         transition: {
-            duration: 2,
-            repeat: Number.POSITIVE_INFINITY,
-            ease: "easeInOut",
-         },
-      },
-   };
-
    return (
       <section
          id="home"
-         className="py-20 md:py-32 flex flex-col items-center justify-center min-h-[90vh]"
+         className="relative py-12  md:py-20 flex flex-col items-center justify-center min-h-[90vh] tech-pattern overflow-hidden"
       >
-         <div className="container px-4 md:px-6 mx-auto">
+         <div className="container px-4 md:px-6 mx-auto relative z-10">
             <motion.div
-               className="flex flex-col items-center space-y-4 text-center"
+               className="flex flex-col items-center space-y-8 text-center"
                variants={containerVariants}
                initial="hidden"
                whileInView="visible"
                viewport={{ once: true, amount: 0.3 }}
             >
-               <motion.div className="space-y-2" variants={itemVariants}>
+               <div className="relative">
+                  <Image
+                     src="/pustamrai.png"
+                     alt="Pustam Rai"
+                     width={600}
+                     height={600}
+                     className="w-30 h-30 md:w-32 md:h-32 object-cover rounded-full transition-all duration-300"
+                  />
+               </div>
+
+               <motion.div className="space-y-4" variants={itemVariants}>
                   <motion.h1
                      className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl/none"
                      variants={itemVariants}
@@ -80,33 +80,39 @@ export default function HeroSection() {
                      </motion.span>
                   </motion.h1>
                   <motion.p
-                     className="mx-auto max-w-[700px] text-xl text-muted-foreground md:text-2xl"
+                     className="mx-auto max-w-[700px] text-xl text-muted-foreground md:text-2xl font-medium"
                      variants={itemVariants}
                   >
-                     <span className="mt-4">
-                        <span>
-                           Full Stack Developer | MERN & Next.js Enthusiast
-                        </span>
-                     </span>
-                     <noscript>
-                        <span>Full Stack Developer | JavaScript Expert</span>
-                     </noscript>
+                     <span className="bg-gradient-to-r from-purple-500 via-indigo-500 to-pink-300 bg-clip-text text-transparent font-semibold">
+                        Full Stack Developer | MERN & Next.js Enthusiast
+                     </span>{" "}
                   </motion.p>
                </motion.div>
 
                <motion.div
-                  className="max-w-[700px] text-muted-foreground"
+                  className="max-w-[700px] text-gray-50 space-y-4"
                   variants={itemVariants}
                >
-                  <p className="text-lg">
+                  <p className="text-lg leading-relaxed">
                      I build fast, scalable, and user-friendly web applications
-                     using Next.js, React, TypeScript, Node.js, Express, and
-                     MongoDB. From crafting smooth frontend experiences to
-                     architecting robust backend systems, I turn ideas into
-                     functional digital solutions.
+                     using{" "}
+                     <span className="text-blue-400 font-medium">Next.js</span>,{" "}
+                     <span className="text-blue-400 font-medium">React</span>,{" "}
+                     <span className="text-blue-400 font-medium">
+                        TypeScript
+                     </span>
+                     ,{" "}
+                     <span className="text-blue-400 font-medium">Node.js</span>,{" "}
+                     <span className="text-blue-400 font-medium">Express</span>,
+                     and
+                     <span className="text-blue-400 font-medium"> MongoDB</span>
+                     . From crafting smooth frontend experiences to architecting
+                     robust backend systems, I turn ideas into functional
+                     digital solutions.
                   </p>
                </motion.div>
 
+               {/* social media */}
                <motion.div
                   className="flex gap-4 mt-6"
                   variants={{
@@ -123,9 +129,13 @@ export default function HeroSection() {
                         variant="ghost"
                         size="icon"
                         asChild
-                        className="hover:scale-110 transition-transform"
+                        className="hover:scale-110 hover:bg-blue-500 hover:text-gray-50 transition-all duration-300 rounded-full"
                      >
-                        <Link href="" target="_blank" rel="noopener noreferrer">
+                        <Link
+                           href="https://github.com/PustamRai"
+                           target="_blank"
+                           rel="noopener noreferrer"
+                        >
                            <Github className="h-5 w-5" />
                            <span className="sr-only">GitHub</span>
                         </Link>
@@ -136,9 +146,13 @@ export default function HeroSection() {
                         variant="ghost"
                         size="icon"
                         asChild
-                        className="hover:scale-110 transition-transform"
+                        className="hover:scale-110 hover:bg-blue-500 hover:text-gray-50 transition-all duration-300 rounded-full"
                      >
-                        <Link href="" target="_blank" rel="noopener noreferrer">
+                        <Link
+                           href="https://www.linkedin.com/in/pustamrai"
+                           target="_blank"
+                           rel="noopener noreferrer"
+                        >
                            <Linkedin className="h-5 w-5" />
                            <span className="sr-only">LinkedIn</span>
                         </Link>
@@ -149,40 +163,15 @@ export default function HeroSection() {
                         variant="ghost"
                         size="icon"
                         asChild
-                        className="hover:scale-110 transition-transform"
+                        className="hover:scale-110 hover:bg-blue-500 hover:text-gray-50 transition-all duration-300 rounded-full"
                      >
-                        <Link href="">
+                        <Link href="mailto:">
                            <Mail className="h-5 w-5" />
                            <span className="sr-only">Email</span>
                         </Link>
                      </Button>
                   </motion.div>
                </motion.div>
-            </motion.div>
-
-            <motion.div
-               className="absolute bottom-10 left-1/2 transform -translate-x-1/2 hidden md:block js-only"
-               variants={floatingVariants}
-               animate="animate"
-               initial={{ opacity: 0 }}
-               whileInView={{ opacity: 1 }}
-               viewport={{ once: true }}
-               transition={{ delay: 1.5, duration: 0.5 }}
-            >
-               <Link
-                  href="#"
-                  onClick={(e) => {
-                     e.preventDefault();
-                     window.scrollTo({ top: 0, behavior: "smooth" });
-                  }}
-               >
-                  <motion.div
-                     whileHover={{ scale: 1.1 }}
-                     whileTap={{ scale: 0.95 }}
-                  >
-                     <ArrowUpCircle className="h-10 w-10 text-primary" />
-                  </motion.div>
-               </Link>
             </motion.div>
          </div>
       </section>
